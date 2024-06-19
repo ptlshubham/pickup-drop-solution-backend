40780cb91f345ccd49dff60bbacbe5fd03d4b543
Shubham Patel <ptlshubham@gmail.com>
2024-06-19 12:32:31 +0530

    remove extra

--- src/db/db.js
+++ src/db/db.js
@@ -5,8 +5,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    // database: "salon-portal"
-    database: "angrez"
+    database: "pds"
 });
 exports.executeSql = function (sql, callback) {
     con.query(sql, function (err, result) {
